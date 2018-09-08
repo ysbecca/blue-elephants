9dc4c527ef866b1c611ede2135218e85d3f8245c
Rebecca Stone <ysbecca@gmail.com>
2018-09-08 13:01:30 +0100

    simple voting js ajax

--- config.py
+++ config.py
@@ -1,2 +1,10 @@
 WTF_CSRF_ENABLED = True
 SECRET_KEY = 'a-very-secret-elephant'
+SESSION_COOKIE_SECURE = True
+
+import os
+
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
+SQLALCHEMY_TRACK_MODIFICATIONS = True   
