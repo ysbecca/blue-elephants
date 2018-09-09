b467d6c24318d617635d4eb20060300e9362150e
Rebecca Stone <ysbecca@gmail.com>
2018-09-09 20:05:49 +0100

    app.run ready for prod

--- config.py
+++ config.py
@@ -7,4 +7,4 @@ import os
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
-SQLALCHEMY_TRACK_MODIFICATIONS = True   
+SQLALCHEMY_TRACK_MODIFICATIONS = True
--- run.py
+++ run.py
@@ -1,4 +1,6 @@
 from app import app
 
+# app.run(debug=True)
 
-app.run(debug=True)
+if __name__ == '__main__':
+    app.run()
